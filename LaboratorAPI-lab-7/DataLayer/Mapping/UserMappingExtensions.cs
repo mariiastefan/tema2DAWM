c7dff1a0db5f3ec29b5fe438e0a28e5f929e5b47
mariiastefan <mariiastefan17@gmail.com>
2023-04-25 19:16:21 +0300

    added UserMapping

---
+++ LaboratorAPI-lab-7/DataLayer/Mapping/UserMappingExtensions.cs
@@ -0,0 +1,35 @@
+﻿using DataLayer.Dtos;
+using DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Mapping
+{
+
+    public static class UserMappingExtensions
+    {
+        public static List<UserDto> ToUserDto(this List<User> users)
+        {
+            var results = users.Select(e => e.ToUserDto()).ToList();
+
+            return results;
+        }
+
+        public static UserDto ToUserDto(this User user)
+        {
+            if (user == null) return null;
+
+            var result = new UserDto();
+            result.Id = user.Id;
+            result.Username = user.Username;
+            result.Email = user.Email;
+            result.RoleId = user.RoleId;
+            result.RoleName = user.Role?.Name;
+
+            return result;
+        }
+    }
+}
