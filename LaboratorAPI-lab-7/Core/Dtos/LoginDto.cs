ab65d4ff75685c3df644af88157f0cae5828375f
mariiastefan <mariiastefan17@gmail.com>
2023-04-25 12:18:15 +0300

    added LoginDto and RegisterDto

---
+++ LaboratorAPI-lab-7/Core/Dtos/LoginDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Dtos
+{
+    public class LoginDto
+    {
+        [Required, MaxLength(100)]
+        public string Username { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Password { get; set; }
+    }
+}
