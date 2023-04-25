b285a48ed8a40bdeea715c6fea34970ab9acc051
mariiastefan <mariiastefan17@gmail.com>
2023-04-25 19:14:29 +0300

    added UserRepo

---
+++ LaboratorAPI-lab-7/DataLayer/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repositories
+{
+    public class UserRepository: RepositoryBase<User>
+    {
+        private readonly AppDbContext dbContext;
+
+        public UserRepository(AppDbContext dbContext) : base(dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public User GetByEmail(string email)
+        {
+            return dbContext.Users.FirstOrDefault(s => s.Email == email);
+        }
+        public User GetByUsername(string username)
+        {
+            return dbContext.Users.FirstOrDefault(s => s.Username == username);
+        }
+    }
+}
